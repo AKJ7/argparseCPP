c58478a1f656f3691450bf464167bba1a03d8f2d
Komi Atchoglo <komi.atchoglo@stud.uni-due.de>
2022-05-10 12:18:00 +0200

    License update

--- include/argparse.hpp
+++ include/argparse.hpp
@@ -1,5 +1,5 @@
   /*
-   * Copyright <2019> <Atchoglo, Komi Jules>
+   * Copyright <2019> <AKJ7>
 
   Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
   following conditions are met:
